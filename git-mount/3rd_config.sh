c6e6cad3a1eb929e69c67c8e14e17e9892e4d9f5
wangbinio <75832671@qq.com>
2025-09-09 13:20:27 +0800

    update

--- git-mount/3rd_config.sh
+++ git-mount/3rd_config.sh
@@ -2,6 +2,7 @@
 cd /home/szzh/maxsim_core/3rdParty && \
 mkdir boost_1_59 && tar xzf boost_1_59.tar.xz -C boost_1_59 && \
 tar xzf libgflags_ft.tar.xz && cp -rd gflags_ft/lib/libgflags* /home/szzh/SSG/Release/ && \
-tar xzf libspdlog_ft.tar.xz && cp -rd spdlog_ft/* /home/szzh/SSG/Release/
+tar xzf libspdlog_ft.tar.xz && cp -rd spdlog_ft/* /home/szzh/SSG/Release/ && \
+tar xzf geographic_ft.tar.gz && cp -rd geographic_ft/lib/libGeographicLib.so* /home/szzh/SSG/Release/
 
 
